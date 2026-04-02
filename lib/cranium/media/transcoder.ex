5080945c9a09718bddb117b7148a0e60a6efeaff
Kevin Gisi <kevin@kevingisi.com>
2026-04-02 02:33:44 +0000

    Update ets setting on transcoder

--- lib/cranium/media/transcoder.ex
+++ lib/cranium/media/transcoder.ex
@@ -11,7 +11,7 @@ defmodule Cranium.Media.Transcoder do
 
   @impl true
   def init(_opts) do
-    stt_cache = :ets.new(:stt_cache, [:set, :read_concurrency])
+    stt_cache = :ets.new(:stt_cache, [:set, read_concurrency: true])
     schedule_sweep()
     Cranium.Events.subscribe()
     {:ok, %{stt_cache: stt_cache}}
@@ -26,20 +26,28 @@ defmodule Cranium.Media.Transcoder do
   end
 
   @impl true
-  def handle_info({:segment_received, %Segment{direction: :inbound} = seg}, %{stt_cache: stt_cache} = state) do
+  def handle_info(
+        {:segment_received, %Segment{direction: :inbound} = seg},
+        %{stt_cache: stt_cache} = state
+      ) do
     cache_key = :erlang.phash2(seg.audio)
+
     case :ets.lookup(stt_cache, cache_key) do
-      [{_, result, _}] -> emit_transcription(result, seg)
+      [{_, result, _}] ->
+        emit_transcription(result, seg)
+
       [] ->
         case Transcriber.process(seg.audio) do
           {:ok, result} ->
             :ets.insert(stt_cache, {cache_key, result, System.monotonic_time(:second)})
             emit_transcription(result, seg)
+
           {:error, reason} ->
             Logger.error("Transcription failed: #{inspect(reason)}")
             emit_transcription_failure(reason, seg)
         end
     end
+
     {:noreply, state}
   end
 
@@ -49,10 +57,14 @@ defmodule Cranium.Media.Transcoder do
   defp schedule_sweep, do: Process.send_after(self(), :sweep, :timer.minutes(1))
 
   defp emit_transcription(text, %Segment{conversation_id: cid}) do
-    Cranium.Events.broadcast({:transcription_complete, %Transcription{text: text, conversation_id: cid}})
+    Cranium.Events.broadcast(
+      {:transcription_complete, %Transcription{text: text, conversation_id: cid}}
+    )
   end
 
   defp emit_transcription_failure(reason, %Segment{conversation_id: cid}) do
-    Cranium.Events.broadcast({:transcription_failed, %Transcription{failure: reason, conversation_id: cid}})
+    Cranium.Events.broadcast(
+      {:transcription_failed, %Transcription{failure: reason, conversation_id: cid}}
+    )
   end
 end
