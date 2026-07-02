17411bc21c075cca045552e1d7a3c7e28931e905
Kevin Gisi <kevin@kevingisi.com>
2026-07-02 19:53:48 +0000

    overlay: pass config.grottoUrl through as GROTTO_URL

--- overlay.nix
+++ overlay.nix
@@ -24,6 +24,7 @@
       "FORT_ORIGIN=dev-sandbox"
       "MACROS_PATH=/home/dev/Projects/hoard/macros"
       "MACROS_STATE_PATH=/home/dev/.local/state/cranium/macros_state.json"
+    ] ++ (if config ? grottoUrl then [ "GROTTO_URL=${config.grottoUrl}" ] else [ ]) ++ [
       "PATH=/run/current-system/sw/bin:/run/overlays/bin:/run/managed-bin:/home/dev/.local/bin"
     ];
   };
